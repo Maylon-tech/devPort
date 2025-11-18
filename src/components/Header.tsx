550aaa58a89d5bd41a51f690cf69e09de66e7457
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-11-18 21:39:12 +0900

    restarting from header

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import Nav from "./Nav"
+import { Button } from "./ui/button"
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        {/* LOGO */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Luke <span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* DECKTOP NAV  --  HIRE ME BUTTON */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
